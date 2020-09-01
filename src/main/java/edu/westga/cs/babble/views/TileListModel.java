8e04cb20d6e3c664c0f43d0c54de84db530afa39
Sofiya Anikina <sanikin1@my.westga.edu>
2020-08-31 22:09:15 -0500

    Updated TileListModel class. Removed unused imports.

--- src/main/java/edu/westga/cs/babble/views/TileListModel.java
+++ src/main/java/edu/westga/cs/babble/views/TileListModel.java
@@ -3,9 +3,6 @@
  */
 package edu.westga.cs.babble.views;
 
-import java.util.List;
-
-import javax.swing.AbstractListModel;
 import javax.swing.ListModel;
 import javax.swing.event.ListDataListener;
 
@@ -15,42 +12,53 @@ import edu.westga.cs.babble.model.TileBag;
 import edu.westga.cs.babble.model.TileRack;
 
 /**
- * @author sofiya
+ * Custom list model that implements the ListModel interface. Has functionality
+ * of TileRack and used as a model for JList.
+ * 
+ * @author Sofiya Anikina
  *
  */
 public class TileListModel extends TileRack implements ListModel<Tile> {
-	TileBag bag;
-	
+	private TileBag bag;
+
 	/**
-	 * Fills the tile rack with a random number of tiles from the bag
-	 * @throws EmptyTileBagException 
+	 * Model's constructor.
+	 * 
+	 * @throws EmptyTileBagException
 	 */
 	public TileListModel() throws EmptyTileBagException {
 		this.bag = new TileBag();
-		int numberOfTilesToDisplay = getNumberOfTilesNeeded();
-		for(int i=0; i < numberOfTilesToDisplay; i++)
-		{
-			if(this.bag.isEmpty()) break;
+		fillRack();
+	}
+
+	/**
+	 * Fills the TileRack with Tiles from the TileBag
+	 * 
+	 * @throws EmptyTileBagException
+	 */
+	public void fillRack() throws EmptyTileBagException {
+		int numberOfTilesToDisplay = this.getNumberOfTilesNeeded();
+		for (int i = 0; i < numberOfTilesToDisplay; i++) {
+			if (this.bag.isEmpty())
+				break;
 			Tile tile;
 			tile = this.bag.drawTile();
-			append(tile);
+			this.append(tile);
 		}
 	}
-	
-	
+
 	@Override
 	public void addListDataListener(ListDataListener l) {
 		// TODO Auto-generated method stub
-		
-		
+
 	}
 
 	@Override
 	public Tile getElementAt(int index) {
-		//return null;
-	
+		// return null;
+
 		return this.getTiles().get(index);
-		//return super.tiles().size();
+		// return super.tiles().size();
 	}
 
 	@Override
@@ -62,7 +70,7 @@ public class TileListModel extends TileRack implements ListModel<Tile> {
 	@Override
 	public void removeListDataListener(ListDataListener l) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
