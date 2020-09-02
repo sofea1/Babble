579a6f966f8fff1d5386183719e12bcee23dd0bc
Sofiya Anikina <sanikin1@my.westga.edu>
2020-09-01 22:17:38 -0500

    Updated the Tile Gui layout in the CellRenderer class.

--- src/main/java/edu/westga/cs/babble/views/TileCellRenderer.java
+++ src/main/java/edu/westga/cs/babble/views/TileCellRenderer.java
@@ -2,9 +2,14 @@ package edu.westga.cs.babble.views;
 
 import java.awt.Color;
 import java.awt.Component;
+
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.ListCellRenderer;
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
+
 import edu.westga.cs.babble.model.Tile;
 
 /**
@@ -12,15 +17,19 @@ import edu.westga.cs.babble.model.Tile;
  * in the view
  * 
  * @author Sofiya Anikina
+ * @version September 1, 2020
  *
  */
 public class TileCellRenderer extends JLabel implements ListCellRenderer<Tile> {
 
 	private static final long serialVersionUID = 1L;
-	private static final Color HIGHLIGHT_COLOR = new Color(0, 0, 128);
+	private Border border;
 
 	public TileCellRenderer() {
 		setOpaque(true);
+		this.border = BorderFactory.createLineBorder(Color.GRAY, 1);
+		setBorder(border);
+		this.setHorizontalAlignment(SwingConstants.CENTER);
 
 	}
 
@@ -34,16 +43,18 @@ public class TileCellRenderer extends JLabel implements ListCellRenderer<Tile> {
 			boolean isSelected, // is the cell selected
 			boolean cellHasFocus) { // does the cell have focus)
 
-		Tile tileTest = (Tile) value;
-		String testStr = String.valueOf(tileTest.getLetter());
-		setText(testStr);
-		setToolTipText(testStr);
-
+		Tile tile = (Tile) value;
+		String tileString = String.valueOf(tile.getLetter());
+		setText(tileString);
+		setToolTipText(tileString);
+		this.setPreferredSize(getMaximumSize());
 		if (isSelected) {
-			setBackground(HIGHLIGHT_COLOR);
-			setForeground(Color.white);
+			setBackground(list.getSelectionBackground());
+			setForeground(list.getSelectionBackground());
+
 		} else {
-			setBackground(Color.white);
+
+			setBackground(list.getBackground());
 			setForeground(Color.black);
 		}
 
